5f597bed0bbdb5bdd4cf28e783888a4aaa46d2e6
Henrique <henriquecolini@gmail.com>
2019-10-29 22:57:42 -0300

    Smooth WASD movement

--- build/game.js
+++ build/game.js
@@ -3,39 +3,55 @@ define(["require", "exports", "./painter/painter", "./painter/drawableHex"], fun
     Object.defineProperty(exports, "__esModule", { value: true });
     class Game {
         constructor() {
+            this.keys = { w: false, a: false, s: false, d: false };
             this.mouse = { x: 0, y: 0 };
             this.pan = { x: 0, y: 0, cx: 0, cy: 0 };
-            this.camMovement = { x: 0, y: 0 };
+            this.camSpeed = 40;
             this._panning = false;
+            this.update = (deltaTime) => {
+                let camMove = { x: 0, y: 0 };
+                camMove.x += this.keys.a ? -1 : 0;
+                camMove.x += this.keys.d ? 1 : 0;
+                camMove.y += this.keys.w ? -1 : 0;
+                camMove.y += this.keys.s ? 1 : 0;
+                if (camMove.x != 0 && camMove.y != 0) {
+                    camMove.y /= Math.SQRT2;
+                    camMove.x /= Math.SQRT2;
+                }
+                this.painter.camera.x += (camMove.x * this.camSpeed) * deltaTime;
+                this.painter.camera.y += (camMove.y * this.camSpeed) * deltaTime;
+            };
             this.handleKeyDown = (evt) => {
-                switch (evt.key) {
-                    case "a":
-                        this.camMovement.x -= 1;
-                        break;
-                    case "d":
-                        this.camMovement.x += 1;
-                        break;
-                    case "w":
-                        this.camMovement.y -= 1;
-                        break;
-                    case "s":
-                        this.camMovement.y += 1;
-                        break;
+                if (!evt.repeat) {
+                    switch (evt.key) {
+                        case "a":
+                            this.keys.a = true;
+                            break;
+                        case "d":
+                            this.keys.d = true;
+                            break;
+                        case "w":
+                            this.keys.w = true;
+                            break;
+                        case "s":
+                            this.keys.s = true;
+                            break;
+                    }
                 }
             };
             this.handleKeyUp = (evt) => {
                 switch (evt.key) {
                     case "a":
-                        this.camMovement.x += 1;
+                        this.keys.a = false;
                         break;
                     case "d":
-                        this.camMovement.x -= 1;
+                        this.keys.d = false;
                         break;
                     case "w":
-                        this.camMovement.y += 1;
+                        this.keys.w = false;
                         break;
                     case "s":
-                        this.camMovement.y -= 1;
+                        this.keys.s = false;
                         break;
                 }
             };
@@ -77,6 +93,14 @@ define(["require", "exports", "./painter/painter", "./painter/drawableHex"], fun
             document.addEventListener('mousedown', this.handleMouseDown);
             document.addEventListener('mouseup', this.handleMouseUp);
             document.addEventListener('mouseenter', this.handleMouseMove);
+            let last = undefined;
+            const frame = (timeStamp) => {
+                let curr = timeStamp;
+                this.update(last ? (curr - last) / 1000 : 1 / 60);
+                last = curr;
+                window.requestAnimationFrame(frame);
+            };
+            window.requestAnimationFrame(frame);
         }
         get panning() { return this._panning; }
         set panning(pan) {
--- src/web-yoy/game.ts
+++ src/web-yoy/game.ts
@@ -5,9 +5,11 @@ export default class Game {
 
 	private painter: Painter;
 
+	private keys = {w: false, a: false, s: false, d: false};
+
 	private mouse = {x: 0, y: 0};		
 	private pan = {x: 0, y: 0, cx: 0, cy: 0};
-	private camMovement = {x: 0, y: 0};
+	private camSpeed = 40;
 
 	private _panning = false;
 
@@ -40,23 +42,56 @@ export default class Game {
 		document.addEventListener('mousedown', this.handleMouseDown);
 		document.addEventListener('mouseup', this.handleMouseUp);
 		document.addEventListener('mouseenter', this.handleMouseMove);
+
+		let last: number = undefined;
+
+		const frame = (timeStamp: DOMHighResTimeStamp) => {
+			let curr = timeStamp;
+			this.update(last ? (curr-last)/1000 : 1/60);
+			last = curr;
+			window.requestAnimationFrame(frame);
+		}
+	
+		window.requestAnimationFrame(frame);
+
+	}
+
+	private update = (deltaTime: number) => {
+
+		let camMove = {x: 0, y: 0};
+
+		camMove.x += this.keys.a ? -1 : 0;
+		camMove.x += this.keys.d ? 1 : 0;
+		camMove.y += this.keys.w ? -1 : 0;
+		camMove.y += this.keys.s ? 1 : 0;
+
+		if (camMove.x != 0 && camMove.y != 0) {
+			camMove.y /= Math.SQRT2;
+			camMove.x /= Math.SQRT2;
+		}
+
+		this.painter.camera.x += (camMove.x*this.camSpeed) * deltaTime;
+		this.painter.camera.y += (camMove.y*this.camSpeed) * deltaTime;
+
 	}
 
 	private handleKeyDown = (evt: KeyboardEvent) => {
-		switch(evt.key) {
-			case "a": this.camMovement.x -= 1; break;
-			case "d": this.camMovement.x += 1; break;
-			case "w": this.camMovement.y -= 1; break;
-			case "s": this.camMovement.y += 1; break;
+		if (!evt.repeat) {
+			switch(evt.key) {
+				case "a": this.keys.a = true; break;
+				case "d": this.keys.d = true; break;
+				case "w": this.keys.w = true; break;
+				case "s": this.keys.s = true; break;
+			}
 		}
 	}
 
 	private handleKeyUp = (evt: KeyboardEvent) => {
 		switch(evt.key) {
-			case "a": this.camMovement.x += 1; break;
-			case "d": this.camMovement.x -= 1; break;
-			case "w": this.camMovement.y += 1; break;
-			case "s": this.camMovement.y -= 1; break;
+			case "a": this.keys.a = false; break;
+			case "d": this.keys.d = false; break;
+			case "w": this.keys.w = false; break;
+			case "s": this.keys.s = false; break;
 		}
 	}
 
